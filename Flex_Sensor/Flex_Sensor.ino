fc8d556265a16b68f2fb62e0ca087cb062faf9ed
FR-Amira <frih.amira1995@gmail.com>
2024-08-19 19:13:35 +0200

    Update Flex_Sensor.ino

--- Flex_Sensor/Flex_Sensor.ino
+++ Flex_Sensor/Flex_Sensor.ino
@@ -2,12 +2,19 @@
 // capteur passif lazemo donne bech ya3m action par exemple ultason yestana single bech yebda ye5dem
 
 // Flex Sensor(fih resistance variable: non polarise) capteur actif 
+#include<Servo.h>
+Servo Sg90;
+int ValFlex;
+
 void setup() {
-  // put your setup code here, to run once:
+  Sg90.attach(5);
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  ValFlex = analogRead(A0);
+ Serial.println(ValFlex);
+ int PwmSp90=map(ValFlex,0,1023,0,180); //0 409/180 113
+     Sg90.write(PwmSp90);
 }
